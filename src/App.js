2426acc9087538e78cd3156c067f05e6e365af27
Ballursajeed <ballursajeed@gmail.com>
2023-11-20 23:18:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import WeatherApp from "./Components/WeatherApp/WeatherApp"
+
+function App() {
+
+  return (
+   <>
+    <div>
+      <WeatherApp />
+    </div>
+   </>
+  )
+}
+
+export default App
